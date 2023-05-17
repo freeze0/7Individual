45e1f0d910fb8e0a70071dc9a13f36501d4579c2
freeze0 <kras2001@bk.ru>
2023-05-17 21:48:06 +0300

    dodelal

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import xmltodict
 with open('7.osm', 'r', encoding='utf-8') as f:
     dct = xmltodict.parse(f.read())
 
-hotels = {}
+k = 0
+hotels = []
 for node in dct['osm']['node']:
     if 'tag' in node:
         tags = node['tag']
         if isinstance(tags, list):
             for tag in tags:
+                if tag['@k'] == "name":
+                    name = tag['@v']
                 if tag['@k'] == 'tourism' and tag['@v'] == 'hotel':
-                    name = node.get('name', 'Unnamed Hotel')
-                    hotel_type = node.get('hotel', 'Unknown')
-                    if hotel_type not in hotels:
-                        hotels[hotel_type] = set()
-                    hotels[hotel_type].add(name)
+                    k+=1
+                    hotels.append(name)
+                elif 'tag' in node and isinstance(node['tag'], dict):
+                    if node['tag']['@k'] == 'hotel':
+                        k+=1
+                        hotels.append(name)
 
-print('Total number of hotels:', sum(len(h) for h in hotels.values()))
-for hotel_type, names in sorted(hotels.items()):
-    print(f"{hotel_type}: {', '.join(sorted(names))}")
+print(k, sorted(hotels))
